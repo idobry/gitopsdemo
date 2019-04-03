6102884debc00aa7351f1afa51f749b4faa3fbe6
ido <idobry@hotmail.co.il>
2019-04-03 15:00:31 +0300

    Update hello-docker-web.go

--- hello-docker-web.go
+++ hello-docker-web.go
@@ -22,7 +22,7 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 
-    fmt.Fprint(w,"Hello from production env\n")
+    fmt.Fprint(w,"Hello from canary" + os.Getenv("ENV") + "\n")
     //fmt.Fprint(w,"**jerusalem == palastine**\n")
     fmt.Fprint(w,"\n")
     fmt.Fprint(w,"                              ##\n")
